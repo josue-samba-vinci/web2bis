de511e4f1c2a076b1055db8952d830f7793002cb
josue-samba-vinci <josue.sambadianzungu@student.vinci.be>
2025-07-04 19:05:18 +0200

    add : files for 1.3

---
+++ exercises/1.3/app.ts
@@ -0,0 +1,25 @@
+import express from "express";
+
+// Importing routers
+import filmsRouter from "./routes/films";
+
+const app = express();
+
+// Middleware to log the number of requests
+let requestCounter = 0;
+app.use((req, _res, next) => {
+  if (req.method === "GET") {
+    requestCounter++;
+    console.log(`GET counter : ${requestCounter}`);
+  }
+  next();
+});
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Mounting the films router
+// This will handle requests to /films
+app.use("/films", filmsRouter);
+
+export default app;
---
+++ exercises/1.3/types.ts
@@ -0,0 +1,13 @@
+interface Film {
+  id: number;
+  title: string;
+  director: string;
+  duration: number;
+  budget?: number;
+  description?: string;
+  imageUrl?: string;
+}
+
+//type NewPizza = Omit<Pizza, "id">;
+
+export type { Film};
